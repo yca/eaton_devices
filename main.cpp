716f68ef9235f43547c8da8b71de3f779ca28196
Yusuf Caglar Akyuz <caglar.akyuz@sparsetechnology.com>
2021-09-26 10:49:36 +0300

    main: start implementing main simulation loop and data collection hub.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
 #include "uuid.h"
-#include <3rdparty/loguru/debug.h>
+#include "device.h"
+#include "transport.h"
+#include "3rdparty/loguru/debug.h"
+#include "hub.h"
+
+#include "transports/udptransport.h"
+#include "transports/tcptransport.h"
+#include "transports/mqtttransport.h"
+
+#include <thread>
+
+class Simulated
+{
+public:
+	enum TransportType {
+		TRANSPORT_UDP,
+		TRANSPORT_TCP,
+		TRANSPORT_MQTT,
+	};
+
+	Simulated(int deviceCount, TransportType ttype)
+	{
+		for (int i = 0; i < deviceCount; i++) {
+			Device *dev = createRandomDevice(ttype);
+			devices.push_back(dev);
+		}
+	}
+
+	int runSimulation(int latencyMs)
+	{
+		while (1) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(latencyMs));
+			for (auto *dev: devices)
+				dev->sendMeasurements();
+		}
+
+		return 0;
+	}
+
+protected:
+	Device * createRandomDevice(TransportType ttype)
+	{
+		/*if (ttype == TRANSPORT_UDP)
+			dev = new Device(new UdpTransport);
+		else if (ttype == TRANSPORT_TCP)
+			dev = new Device(new UdpTransport);*/
+		return nullptr;
+	}
+
+protected:
+	std::vector<Device *> devices;
+};
 
 int main()
 {
@@ -10,7 +61,20 @@ int main()
 	if (var)
 		loguru::g_stderr_verbosity = std::stoi(var);
 
-	gWarn("getting started");
+	/* TODO: add command line parameters */
+	int devcnt = 100;
+	int latms = 50;
+	Simulated::TransportType ttype = Simulated::TRANSPORT_MQTT;
 
-	return 0;
+	/* start log collection using hub instance */
+	Hub hub;
+	int err = hub.startTcp(21569);
+	if (err) {
+		gWarn("Error '%d' starting hub, aborting simulation.", err);
+		return err;
+	}
+
+	/* create and start simulation */
+	Simulated sim(devcnt, ttype);
+	return sim.runSimulation(latms);
 }
